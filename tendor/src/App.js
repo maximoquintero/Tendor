ca777b6f3ece171bf92226c9ec6c096578326844
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2024-03-14 06:35:49 -0600

    Creadas las rutas

--- tendor/src/App.js
+++ tendor/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from './pages/Login';
+import Registro from './pages/Registro';
+import Dashboard from './pages/Dashboard';
+import Historial from './pages/Historial';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Login/>
+    },
+    {
+      path:"/registro",
+      element: <Registro/>
+    },
+    {
+      path:"/dashboard",
+      element: <Dashboard/>
+    },
+    {
+      path:"/historial",
+      element: <Historial/>
+    },
+    
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
