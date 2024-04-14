41c58258f6cb8995eb4335a7935acd530597557b
Goraz23 <angomezs.2303@gmail.com>
2024-04-14 09:25:07 -0500

    Modo Oscuro

---
+++ Tendor/frontend_tendor/src/components/Btn_ModeDark.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+function Btn_ModeDark() {
+  const [darkMode, setDarkMode] = useState(getInitialMode());
+
+  // Función para obtener el modo oscuro desde el almacenamiento local
+  function getInitialMode() {
+    const savedMode = JSON.parse(localStorage.getItem("darkMode"));
+    return savedMode || false; // Devuelve false si no se encuentra en el almacenamiento local
+  }
+
+  // Función para cambiar entre el modo claro y oscuro
+  function toggleDarkMode() {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    localStorage.setItem("darkMode", JSON.stringify(newMode));
+  }
+
+  // Efecto para aplicar el modo oscuro al cargar la página
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  return (
+    <>
+    <button onClick={toggleDarkMode} className={` ml-10 pt-1 px-1  mt-10 rounded-[100%] ${darkMode ? 'dark' : ''}`}>
+          {darkMode ?  <box-icon name='sun' type='solid' color='#858585'></box-icon> : <box-icon name='moon' type='solid' color='#858585'></box-icon>}
+    </button>
+    </>
+  )
+}
+
+export default Btn_ModeDark;
--- Tendor/frontend_tendor/src/components/Tupla.js
+++ Tendor/frontend_tendor/src/components/Tupla.js
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function Tupla({nombre_tupla, dato, value,change,descripcion}) {
+function Tupla({ nombre_tupla, dato, value, change, descripcion, darkMode }) {
   return (
     <>
       <div className='pb-4'>
-      <label className="text-black font-bold dark:text-white ml-1 flex" htmlFor="nombre">
-            {nombre_tupla}
-          </label>
-          <input
-            id={nombre_tupla}
-            type={dato}
-            value={value}
-            onChange={change}
-            className="mt-2 h-8 block w-full border-[2px] rounded-2xl border-[#A5A5FD] dark:border-[#858585] p-3 bg-gray-50 dark:bg-[#1F1F1F]"
-            placeholder={descripcion}
-            name={nombre_tupla}
-          />
+        <label className={`font-bold ${darkMode ? 'text-white' : 'text-black'} ml-1 flex`} htmlFor={nombre_tupla}>
+          {nombre_tupla}
+        </label>
+        <input
+          id={nombre_tupla}
+          type={dato}
+          value={value}
+          onChange={change}
+          className={`mt-2 h-8 block w-full border-[2px] rounded-2xl border-${darkMode ? '[#858585]' : '[#A5A5FD]'} dark:border-[#858585] p-3 ${darkMode ? 'bg-[#858585] text-white' : 'bg-gray-50 text-black'}`}
+          placeholder={descripcion}
+          name={nombre_tupla}
+          style={{ backgroundColor: darkMode ? '#000' : 'transparent' }}
+        />
       </div>
     </>
-  )
+  );
 }
 
 export default Tupla;
